90b7d1d78ca0aad3f7cc21ac5194910eed34ca55
cbianca168 <carolynwhiteb@gmail.com>
2024-07-09 11:32:21 -0400

    Create follow.ts

---
+++ follow.ts
@@ -0,0 +1,52 @@
+namespace follower {
+  #enums
+  let follower: Sprite = null;
+  let target: Sprite = null;
+  let followDistanc = 0;
+
+  /**
+    * Set the follower sprite
+    */
+  //% blockId = set_follower block = "set follower %sprite"
+  //% weight = 100
+  export function setFollower(sprite: Sprite) {
+    follower = sprite;
+  }
+
+  /**
+    * Set the target sprite
+    */
+  //% blockId = set_target block = "set target %sprite"
+  //% weight = 90
+  export function setTarget(sprite: Sprite) {
+    target = sprite;
+  }
+
+  /**
+    * Set the follow distance
+    */
+  //% blockId = set_distance block = "set follow distance %distance"
+  //% weight = 80
+  export function setDistance(distance: number) {
+    followDistance = distance;
+  }
+  
+  /**
+    * Update the follower position
+    */
+  //* blockId = update_follower block = "update follower"
+  //% weight = 70
+  export function updateFollower() {
+    if (follower && target) {
+      let distance = Math.sqrt(Math.pow(follower.x - target.x, 2) + Math.pow(follower.y - target.y, 2));
+      if (distance > followDistance) {
+        let angle = Math.atan2(target.y - follower.y, target.x - follower.x);
+        follower.vx = Math.cos(angle) * 50;
+        follower.vy = Math.sin(angle) * 50;
+      } else {
+        follower.vx = 0;
+        follower.vy = 0;
+      }
+    }
+  }
+}
